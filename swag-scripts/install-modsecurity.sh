9e52baf2b6c5844ecf1e426b44f4d543f43161db
Richard Leszczynski <59875350+RichardJRL@users.noreply.github.com>
2023-02-17 16:35:24 +0000

    document issues with configure output

--- swag-scripts/install-modsecurity.sh
+++ swag-scripts/install-modsecurity.sh
@@ -28,8 +28,8 @@
 # Install tooling required to build ModSecurity
 apk add autoconf automake build-base ca-certificates gcc git libtool linux-headers pkgconf valgrind wget
 # Install dependencies of ModSecurity
-apk add afl++ bison curl curl-dev flex gawk geoip-dev libpcrecpp libxml2 libxml2-dev lmdb-dev lua5.3-dev openssl-dev pcre-dev yajl yajl-dev zlib zlib-dev
-# Install tooling required for generating documentation for ModSecurity
+apk add afl++ bison curl curl-dev flex gawk geoip-dev libfuzzy2 libfuzzy2-dev libpcrecpp libxml2 libxml2-dev libxslt libxslt-dev lmdb lmdb-dev lua5.3-dev openssl-dev pcre-dev yajl yajl-dev zlib zlib-dev
+# OPTIONAL: Install tooling required for generating documentation for ModSecurity
 # apk add doxygen 
 
 # Gleaned from the Linode instructions for compiling ModSecurity for Ubuntu 18.04 but AFAIK not needed here (no errors in ./build.sh  nor ./configure) 
@@ -45,14 +45,17 @@ apk add afl++ bison curl curl-dev flex gawk geoip-dev libpcrecpp libxml2 libxml2
 #           : TODO: Test whether expat, expat-dev can be used INSTEAD of libxml2, libxml2-dev
 # libpcrecpp    : apk comment: C++ bindings for PCRE
 #               : URL: https://www.pcre.org/
-#               : TODO: Check if installed by default before running this script
+#               : TODO: Check if installed by default before running this script, because it is now.
 # musl-locales  : apk comment: Locales support for musl
 # libxslt   : apk comment: XML stylesheet transformation library
 #           : URL: http://xmlsoft.org/XSLT/
+#           : NOTES: Only the CentOS and AWS Linux build recipies in the ModSecurity Wiki include this as a named dependency.
 #           : TODO: Check if installed by default before running this script
 #           : ANSWER: libxslt is installed now, libxslt-dev is not.
+#                   : nginx is compiled with xslt support (ref: nginx -V)
 # libgd     : apk comment: Library for the dynamic creation of images by programmers (libraries)
 #           : URL: https://libgd.github.io/
+#           : NOTES: Only the CentOS and AWS Linux build recipies in the ModSecurity Wiki include this as a named dependency.
 #           : TODO: Check if installed by default before running this script
             # ANSWER: Installed now but...
 
@@ -93,9 +96,6 @@ cd /opt
 git clone https://github.com/SpiderLabs/ModSecurity
 cd ./ModSecurity
 
-# Inititalise the ModSecurity git submodule
-
-
 ./build.sh
 ./configure
 # Configure messages of note:
@@ -126,14 +126,28 @@ cd ./ModSecurity
 #    + Treating pm operations as critical section    ....disabled
 
 # Notes on configure output
-# - SSDEEP not found despite ssdeep and ssdeep-static being installed
+
+# afl fuzzer: apk comment: Fuzzer relying on genetic algorithms instead of brute force
+#           : No mention of this in alipine's nginx -V configure options.
+#           : No mention of this in the compilation recipes page on the ModSecurity Wiki
+#           : I don't know what it is or why or where it is disabled.
+# lmdb      : apk comment: Lightning Memory-Mapped Database
+#           : Centos, Amazon & Ubuntu compilation recipies on the ModSecurity wiki all use this.
+#           : It isn't mentioned in the nginx -V configure options
+#           : Appears disabled by default despite lmdb and lmdb-dev being installed
+# pcre2     : pcre2 support is specifically disabled in alpine's nginx configure options, uses pcre instead
+# SSDEEP    : Not found despite ssdeep and ssdeep-static being installed.
+#           : It requires libfuzzy2-dev instead - which appears to be an alternative & preferred implementation.
+
+# Inititalise the ModSecurity git submodule
 git submodule init
 git submodule update
 ./configure
 make -j $(grep -m 1 siblings /proc/cpuinfo | awk -F ':' '{print $2}')
 
-
+################################################################################
 # ModSecurity Connector Build
+################################################################################
 
 cd /opt
 git clone https://github.com/SpiderLabs/ModSecurity-nginx
