aa9ae625bb61e5a61b4dc31256b3aeb0e4d9f3c3
Richard Leszczynski <59875350+RichardJRL@users.noreply.github.com>
2023-02-24 14:22:54 +0000

    Move most comments from the installation script to the readme

--- swag-scripts/install-modsecurity.sh
+++ swag-scripts/install-modsecurity.sh
@@ -29,65 +29,6 @@ apk add bison curl curl-dev flex gawk geoip-dev libfuzzy2 libfuzzy2-dev libpcrec
 # docdir='/docs'
 # mkdir -p "$docdir"
 
-# Gleaned from the Linode instructions for compiling ModSecurity for Ubuntu 18.04 but AFAIK not needed here (no errors in ./build.sh  nor ./configure) 
-# iputils   : apk comment: IP Configuration Utilities (and Ping)
-#           : URL: https://github.com/iputils/iputils/
-#           : Official description: The iputils package is set of small useful utilities for Linux networking.
-#           : TODO: Check if this is installed by default on alpine.
-#           : ANSWER: No, it is not
-# expat-dev : apk comment: XML Parser library written in C
-#           : URL: https://libexpat.github.io
-#           : Official description:  A stream-oriented XML parser library written in C.
-#           : Expat excels with files too large to fit RAM, and where performance and flexibility are crucial.
-#           : TODO: Test whether expat, expat-dev can be used INSTEAD of libxml2, libxml2-dev
-# libpcrecpp    : apk comment: C++ bindings for PCRE
-#               : URL: https://www.pcre.org/
-#               : TODO: Check if installed by default before running this script, because it is now.
-# musl-locales  : apk comment: Locales support for musl
-# libxslt   : apk comment: XML stylesheet transformation library
-#           : URL: http://xmlsoft.org/XSLT/
-#           : NOTES: Only the CentOS and AWS Linux build recipies in the ModSecurity Wiki include this as a named dependency.
-#           : TODO: Check if installed by default before running this script
-#           : ANSWER: libxslt is installed now, libxslt-dev is not.
-#                   : nginx is compiled with xslt support (ref: nginx -V)
-# libgd     : apk comment: Library for the dynamic creation of images by programmers (libraries)
-#           : URL: https://libgd.github.io/
-#           : NOTES: Only the CentOS and AWS Linux build recipies in the ModSecurity Wiki include this as a named dependency.
-#           : TODO: Check if installed by default before running this script
-            # ANSWER: Installed now but...
-
-# Dependency list from https://www.linode.com/docs/guides/securing-nginx-with-modsecurity/
-# Dependencies originally for Ubuntu 18.04
-# list of Alpine dependencies and their Ubuntu 18.04 equivalents 
-# bison \ # ok
-# build-base \ # build-essential: build-base
-# ca-certificates \ # ok
-# curl \ # ok
-# autoconf \ # dh-autoreconf: autoconf
-# doxygen \ # ok
-# flex \ # ok
-# gawk \ # ok
-# iputils \ # iputils-ping: iputils
-# curl-dev \ # libcurl4-gnutls-dev: curl-dev AND MAYBE gnutls-dev
-# expat-dev \ # libexpat1-dev: expat-dev
-# geoip-dev \ # libgeoip-dev: geoip-dev
-# lmdb-dev \ # liblmdb-dev: lmdb-dev
-# pcre2-dev \ # libpcre3-dev (8.44): pcre-dev (8.45) OR pcre2-dev (10.42) (first try with pcre2-dev as the 8.X versions are EOL. NO it wants **pcre-dev**, tested.) (in Debian/Ubuntu, libpcre2... is newer than libpcre3...)
-# libpcre++-dev \ # libpcre++-dev: libpcrecpp
-# openssl-dev \ # libssl-dev (OpenSSL project): 
-# libtool \ # ok
-# libxml2 \ # ok
-# libxml2-dev \ # ok
-# yajl-dev \ # libyajl-dev: yajl-dev
-# musl-locales \ # locales: musl-locales
-# lua5.3-dev \ # ok
-# pkgconf \ # pkg-config: pkgconf
-# wget \ # ok
-# zlib-dev \ # zlib1g-dev: zlib-dev (NB: see also zlib-ng (new gen) vs zlib1g (first gen))
-# zlib \ # zlibc: zlib NB: unsure of the relationship between zlib and zlibc
-# libxslt \ # ok
-# libgd-dev # libgd-dev: libgd
-
 # Get the ModSecurity git repository
 cd /opt
 git clone --depth=1 https://github.com/SpiderLabs/ModSecurity
@@ -100,10 +41,7 @@ git submodule update
 # Pre-build checks etc.
 ./build.sh
 # Run configure without any specific options. Run `./configure --help` for a full list of options.
-# ./configure
-
-#/ Run configure with the GNU linker
-./configure --with-gnu-ld
+./configure
 
 # TODO: Find out what --enable-parser-generation does.
 # ./configure --enable-parser-generation 
@@ -130,38 +68,6 @@ git submodule update
 #     --pdfdir="$docdir"/pdf \
 #     --psdir="$docdir"/ps
 
-
-# ##Configure output of note:
-# configure: LMDB is disabled by default.
-# checking if libcurl is linked with gnutls... no
-# checking for string... no
-# checking for iostream... no
-# checking for dlltool... no
-# checking for sysroot... no
-# checking for mt... no
-# checking if : is a manifest tool... no
-# checking if gcc supports -fno-rtti -fno-exceptions... no
-# checking whether -lc should be explicitly linked in... no
-# checking for shl_load... no
-# checking for shl_load in -ldld... no
-# checking whether a statically linked program can dlopen itself... no
-
-#    + LMDB                                          ....disabled
-#    + PCRE2                                          ....disabled
-#    + Treating pm operations as critical section    ....disabled
-
-# Notes on configure output
-
-# lmdb      : apk comment: Lightning Memory-Mapped Database
-#           : Centos, Amazon & Ubuntu compilation recipes on the ModSecurity wiki all use this.
-#           : It isn't mentioned in the nginx -V configure options
-#           : Appears disabled by default despite lmdb and lmdb-dev being installed
-
-# pcre2     : pcre2 support is specifically disabled in alpine's nginx configure options, uses pcre instead
-
-# SSDEEP    : Not found despite ssdeep and ssdeep-static being installed.
-#           : It requires libfuzzy2-dev instead - which appears to be an alternative & preferred implementation.
-
 # Build on all CPU threads
 make -j $(grep -m 1 siblings /proc/cpuinfo | awk -F ':' '{print $2}')
 make install
@@ -180,17 +86,11 @@ tar -xvzmf "$NGINX_VERSION.tar.gz"
 cd "$NGINX_VERSION"
 
 NGINX_CONFIGURATION_ARGS=$(nginx -V 2> >(sed -n 's/configure arguments: //p'))
-./configure --add-dynamic-module=../ModSecurity-nginx "$NGINX_CONFIGURATION_ARGS"
-
-## Choice lines from ./configure output
-# dirname: unrecognized option '--sbin-path=/usr/sbin/nginx'
-# Try 'dirname --help' for more information.
-# dirname: unrecognized option '--sbin-path=/usr/sbin/nginx'
-# Try 'dirname --help' for more information.
-# Configuration summary
-#   + using system PCRE library
-#   + OpenSSL library is not used
-#   + using system zlib library
+# "Not Binary Compatibile" error suggestions:
+# - Try removing other dynamic modules from the configuration args (no effect)
+# - Try with/without the `--with-compat` option (no effect)
+NGINX_CONFIGURATION_ARGS=$(echo $NGINX_CONFIGURATION_ARGS | sed -E 's/--add-dynamic-module=\/.*\/ ?//g')
+./configure --with-compat "$NGINX_CONFIGURATION_ARGS" --add-dynamic-module=../ModSecurity-nginx
 
 make modules
 mkdir -p /etc/nginx/modules
@@ -199,9 +99,9 @@ mkdir -p /etc/nginx/modules
 # are one-liners that serve to load the nginx module from the /usr/lib/nginx/modules/ directory
 # e.g. load_module "modules/ngx_http_perl_module.so";
 # NB: This module has also been `make install`ed to `/var/lib/nginx/modules/`
+strip objs/ngx_http_modsecurity_module.so
 cp objs/ngx_http_modsecurity_module.so /usr/lib/nginx/modules/
 echo 'load_module "modules/ngx_http_modsecurity_module.so";' > /etc/nginx/modules/10_http_modsecurity.conf
-# TODO: Check for a modules-enabled directory...
 
 ################################################################################
 # Install the OWASP Core Rule Set for ModSecurity
@@ -381,4 +281,6 @@ done
 ################################################################################
 # Download the GeoIP Location Database 
 ################################################################################
-# TODO: Find out which database (country, city etc)
+# TODO: Find out which database (country, city etc) is needed
+# TODO: Find out if the LinuxServer.io container addons for GeoIP or MaxMind
+# satisfy ModSecurity's geolocation database requirements.
